2d7514131dcc654108c8ed7974be19916c51052e
cyuyanshishenmegui <1227641660@qq.com>
2018-12-17 23:49:16 +0800

    equation add commit

--- equation.c
+++ equation.c
@@ -1,4 +1,21 @@
-//求解如下方程组
-//ax + by = c
-//dx + ey = f
-//注意输入/输出，数据类型
+#include <stdio.h>
+int main (){
+	double a,b,c,d,e,f,x,y;
+	printf("a=");
+	scanf("%lf",&a);
+	printf("b=");
+	scanf("%lf",&b);
+	printf("c=");
+	scanf("%lf",&c);
+	printf("d=");
+	scanf("%lf",&d);
+	printf("e=");
+	scanf("%lf",&e);
+	printf("f=");
+	scanf("%lf",&f);
+	x=(c*e-b*f )/(e*a-b*d);
+	y=(c*d-a*f)/(b*d-a*e);
+	printf("x=%lf\n",x);
+	printf("y=%lf",y);
+	return 0; 
+}
